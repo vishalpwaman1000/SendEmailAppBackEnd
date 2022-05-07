f6f3e8421922cc854d10b0b661f075c81622e549
vishalpwaman1000 <95186426+vishalpwaman1000@users.noreply.github.com>
2022-05-07 22:27:28 +0530

    Add files via upload

---
+++ SendEmailApllicationBE/Controllers/SendEmailController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SendEmailApllicationBE.Model;
+using SendEmailApllicationBE.ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendEmailApllicationBE.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class SendEmailController : ControllerBase
+    {
+        public readonly ISendEmailSL _sendEmailSL;
+        public SendEmailController(ISendEmailSL sendEmailSL)
+        {
+            _sendEmailSL = sendEmailSL;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmailAPI(SendEmailAPIRequest request)
+        {
+            SendEmailAPIResponse response = new SendEmailAPIResponse();
+
+            try
+            {
+
+                response = await _sendEmailSL.SendEmailAPI(request);
+            }
+            catch(Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = "Exception Occurs : " + ex.Message;
+            }
+
+            return Ok(response);
+        }
+
+    }
+}
---
+++ SendEmailApllicationBE/Model/SendEmailAPI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendEmailApllicationBE.Model
+{
+    public class SendEmailAPIRequest
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+
+    public class SendEmailAPIResponse
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+}
